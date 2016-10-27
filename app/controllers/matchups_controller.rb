2c1fdfad7526a3f0788fb4b69b8cebb39f03d351
github username <github email address>
2016-10-27 17:26:17 -0400

    goddamn datetimes

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -1,9 +1,9 @@
 class MatchupsController < ApplicationController
 
   def new
-    @matchup = Matchup.new
     @competitors = Competitor.all
     @types = MatchupType.all
+    @matchup = Matchup.new
   end
 
   def index
@@ -11,7 +11,12 @@ class MatchupsController < ApplicationController
   end
 
   def create
-    @matchup = Matchup.new
+    if same_competitors? #|| !valid_date?
+      redirect_to new_matchup_path
+    else
+      #@matchup = Matchup.new
+    end
+    #@matchup = Matchup.new
 
   end
 
@@ -23,6 +28,32 @@ class MatchupsController < ApplicationController
     @competitor = Competitor.find(params[:id])
     @competitor.destroy
     redirect_to competitors_path
+  end
+
+  private
+
+  def competitor1
+    params[:competitor1]
+  end
+
+  def competitor2
+    params[:competitor2]
+  end
+
+  # def valid_date?
+  #
+  #   if @deadline && @deadline > DateTime.now
+  #     return true
+  #   else
+  #     flash[:date] = "You've entered invalid date!"
+  #   end
+  #
+  # end
+
+  def same_competitors?
+    if competitor1 == competitor2
+      flash[:failed_creation] = "You cannot pick the same competitor for a matchup."
+    end
   end
 
 
