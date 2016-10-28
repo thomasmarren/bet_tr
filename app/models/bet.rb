3b8b88de0a2f61a4ed2e0a7b6b2c915ccc243935
Robert Andrew Lin <github email address>
2016-10-28 14:27:58 -0400

    displays all bets and add methods to bets model

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,13 @@
 class Bet < ActiveRecord::Base
   belongs_to :user
   belongs_to :matchups_competitor
+
+  def matchup
+    MatchupsCompetitor.find(self.matchups_competitors_id).matchup
+  end
+
+  def competitor
+    MatchupsCompetitor.find(self.matchups_competitors_id).competitor
+  end
+
 end
--- app/models/matchups_competitor.rb
+++ app/models/matchups_competitor.rb
@@ -2,4 +2,5 @@ class MatchupsCompetitor < ActiveRecord::Base
   belongs_to :matchup
   belongs_to :competitor
   has_many :bets
+
 end
