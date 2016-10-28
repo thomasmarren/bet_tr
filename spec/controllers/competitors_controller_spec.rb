7ea9895e9ac9096e31e72ebf5293c9d6e73a71c0
Thomas Marren <tmarren2090@gmail.com>
2016-10-28 11:53:41 -0400

    add first test

---
+++ spec/controllers/competitors_controller_spec.rb
@@ -0,0 +1,7 @@
+# require 'rails_helper'
+#
+# RSpec.describe CompetitorsController, type: :controller do
+#
+#   let(:)
+#
+# end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  let(:user) {User.create(name: "Bob", username: "bobsburgers", password: "password")}
+
+  describe '#balance' do
+    it 'initializes with blance of 100' do
+      expect(user.balance).to eq(100)
+    end
+  end
+
+end
