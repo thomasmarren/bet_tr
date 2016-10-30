32b3f13f80121dc6cc99db21d83cd1c1c7d8bb48
Thomas Marren <tmarren2090@gmail.com>
2016-10-30 12:25:09 -0400

    ability to see all matchups for a competitor. ability to see all users who have bet on a competitor

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -4,4 +4,25 @@ class Competitor < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name, message: "Competitor already exists"
+
+
+  def bets
+    Bet.find_by_sql(
+    <<-SQL
+    SELECT *
+    FROM bets
+    JOIN matchups_competitors
+    ON bets.matchups_competitors_id = matchups_competitors.id
+    JOIN competitors
+    ON matchups_competitors.competitor_id = competitors.id
+    WHERE competitors.id = #{self.id}
+    SQL
+    )
+  end
+
+  def users_who_bet
+    user_ids = bets.map(&:user_id).uniq
+    users = User.find(user_ids)
+  end
+
 end
