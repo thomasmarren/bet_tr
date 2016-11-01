6ac9235b3ef9bc2772df60800b969844161d84a7
Thomas Marren <tmarren2090@gmail.com>
2016-11-01 14:13:08 -0400

    add win percentage to each user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,10 @@ class User < ActiveRecord::Base
     self.balance ||= 100
   end
 
+  def winning_bets
+    # should find a way to make these find by UNIQUE matchups to prevent spamming
+    # small bets to rack up "wins"
+    Bet.find_by_sql("SELECT user_id, count(matchups_competitors.winner) as won FROM bets JOIN matchups_competitors ON bets.matchups_competitor_id = matchups_competitors.id JOIN users ON bets.user_id = users.id WHERE matchups_competitors.winner = true AND user_id = #{self.id} GROUP BY bets.user_id")
+  end
+
 end
